0c2b7c838a90d0259b78875fbbe5c94818d242bd
uptonking <jinyaoo86@gmail.com>
2024-11-05 11:53:44 +0800

    chore: remove .1024 config

--- script.mjs
+++ script.mjs
@@ -2,15 +2,15 @@ export const hello = 'world '
 console.log('; log ', hello)
 
 export const hello2 = 'world '
-console.log('; log ', hello)
+console.log('; log ', hello2)
 
 export const hello3 = 'world '
-console.log('; log ', hello)
+console.log('; log ', hello3)
 
 export const hello4 = 'world'
-console.log('; log ', hello)
+console.log('; log ', hello4)
 
 export const hello5 = 'world'
-console.log('; log ', hello)
+console.log('; log ', hello5)
 
 
