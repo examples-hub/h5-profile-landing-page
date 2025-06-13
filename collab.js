ee34f51bd2d1c3a6d82287206bb6ffa5ee6526fc
Clacky <develop@clacky.ai>
2025-06-12 23:13:08 -0400

    chore: relative imports

---
+++ collab.js
@@ -0,0 +1,7 @@
+export const hello = 'world '
+console.log('; log ', hello)
+
+export const hello2 = 'world '
+console.log('; log ', hello2)
+
+
---
+++ collab.test.js
@@ -0,0 +1,3 @@
+import { hello2 } from './collab.js';
+
+console.log(';; log ', hello2);
